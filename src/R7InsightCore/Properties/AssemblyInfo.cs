44ec1458570b6d9f5037caef53abab5410b567a7
stopal <stopal@rapid7.com>
2017-06-13 10:54:17 +0100

    LOG-8948 r7 added to project name

--- src/R7InsightCore/Properties/AssemblyInfo.cs
+++ src/R7InsightCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("R7InsightLog4net")]
-[assembly: AssemblyDescription("Rapid7 Insight logging support log4net library")]
+[assembly: AssemblyTitle("R7Insight.Core")]
+[assembly: AssemblyDescription("Rapid7 Insight logging core library for dotnet support")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rapid7")]
-[assembly: AssemblyProduct("R7InsightLog4net")]
+[assembly: AssemblyProduct("R7Insight.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44fdf6b1-1918-4af1-814c-da387736b675")]
+[assembly: Guid("14055980-6937-4745-9449-dabf47c1d892")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.2.0")]
-[assembly: AssemblyFileVersion("2.6.2.0")]
+[assembly: AssemblyVersion("2.6.7.0")]
+[assembly: AssemblyFileVersion("2.6.7.0")]
