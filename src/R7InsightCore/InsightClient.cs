8aebe92e9dbce7853f93cd0fa3de272ab4604268
Szymon Bialkowski <szymon_bialkowski@rapid7.com>
2024-06-13 16:57:27 +0100

    LOG-21638 Security fixes

--- src/R7InsightCore/InsightClient.cs
+++ src/R7InsightCore/InsightClient.cs
@@ -9,7 +9,7 @@ namespace InsightCore.Net
     class InsightClient
     {
         // Rapid7 Insight API server address. 
-        protected const String LeDataUrl = "{0}.data.logs.insight.rapid7.com";
+        protected const string LeDataUrl = "{0}.data.logs.insight.rapid7.com";
 
         // Port number for logging on Rapid7 Insight DATA server. 
         protected const int LeUnsecurePort = 80;
@@ -17,11 +17,8 @@ namespace InsightCore.Net
         // Port number for SSL logging on Rapid7 Insight DATA server. 
         protected const int LeSecurePort = 443;
 
-        // Creates InsightClient instance. If do not define useServerUrl and/or useOverrideProt during call
-        // will be configured to work with the defined server on defined port.
-        public InsightClient(bool useSsl, bool useDataHub, String serverAddr, int port, String region)
+        public InsightClient(bool useSsl, bool useDataHub, string serverAddr, int port, string region)
         {
-            // Override port number and server address to send logs to DataHub instance.
             if (useDataHub)
             {
                 m_UseSsl = false; // DataHub does not support receiving log messages over SSL for now.
@@ -31,13 +28,8 @@ namespace InsightCore.Net
             else
             {
                 m_UseSsl = useSsl;
-
-                if (!m_UseSsl)
-                    TcpPort = LeUnsecurePort;
-                else
-                    TcpPort = LeSecurePort;
-
-                ServerAddr = String.Format(LeDataUrl, region);
+                TcpPort = m_UseSsl ? LeSecurePort : LeUnsecurePort;
+                ServerAddr = string.Format(LeDataUrl, region);
             }
         }
 
@@ -46,72 +38,38 @@ namespace InsightCore.Net
         private TcpClient m_Client = null;
         private Stream m_Stream = null;
         private SslStream m_SslStream = null;
-        public String ServerAddr { get; private set; }
+        public string ServerAddr { get; private set; }
 
-        private Stream ActiveStream
+        private Stream ActiveStream => m_UseSsl ? m_SslStream : m_Stream;
+
+        public void SetSocketKeepAliveValues(TcpClient tcpc, int keepAliveTime, int keepAliveInterval)
         {
-            get
-            {
-                return m_UseSsl ? m_SslStream : m_Stream;
-            }
-        }
+            uint dummy = 0;
+            byte[] inOptionValues = new byte[Marshal.SizeOf(dummy) * 3];
+            bool onOff = true;
 
-        public void SetSocketKeepAliveValues(TcpClient tcpc, int KeepAliveTime, int KeepAliveInterval)
-        {
-            //KeepAliveTime: default value is 2hr
-            //KeepAliveInterval: default value is 1s and Detect 5 times
+            BitConverter.GetBytes((uint)(onOff ? 1 : 0)).CopyTo(inOptionValues, 0);
+            BitConverter.GetBytes((uint)keepAliveTime).CopyTo(inOptionValues, Marshal.SizeOf(dummy));
+            BitConverter.GetBytes((uint)keepAliveInterval).CopyTo(inOptionValues, Marshal.SizeOf(dummy) * 2);
 
-            uint dummy = 0; //lenth = 4
-            byte[] inOptionValues = new byte[System.Runtime.InteropServices.Marshal.SizeOf(dummy) * 3]; //size = lenth * 3 = 12
-            bool OnOff = true;
-
-            BitConverter.GetBytes((uint)(OnOff ? 1 : 0)).CopyTo(inOptionValues, 0);
-            BitConverter.GetBytes((uint)KeepAliveTime).CopyTo(inOptionValues, Marshal.SizeOf(dummy));
-            BitConverter.GetBytes((uint)KeepAliveInterval).CopyTo(inOptionValues, Marshal.SizeOf(dummy) * 2);
-            // of course there are other ways to marshal up this byte array, this is just one way
-            // call WSAIoctl via IOControl
-
-            // .net 1.1 type
-            //int SIO_KEEPALIVE_VALS = -1744830460; //(or 0x98000004)
-            //socket.IOControl(SIO_KEEPALIVE_VALS, inOptionValues, null); 
-
-            // .net 3.5 type
             tcpc.Client.IOControl(IOControlCode.KeepAliveValues, inOptionValues, null);
         }
 
         public void Connect()
         {
             m_Client = new TcpClient();
-#if NETSTANDARD1_3
-            m_Client.ConnectAsync(ServerAddr, TcpPort).Wait();
-#else
             m_Client.Connect(ServerAddr, TcpPort);
-#endif
             m_Client.NoDelay = true;
 
-            // on Azure sockets will be closed after some minutes idle.
-            // which for some reason messes up this library causing it to lose data.
-
-            // turn on keepalive, to keep the sockets open.
-            // I don't really understand why this helps the problem, since the socket already has NoDelay set
-            // so data should be sent immediately. And indeed it does appear to be sent promptly when it works.
             m_Client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
 
-            // set timeouts to 10 seconds idle before keepalive, 1 second between repeats,
             try
             {
-#if NETSTANDARD1_3
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    SetSocketKeepAliveValues(m_Client, 10 * 1000, 1000);
-                }
-#else
                 SetSocketKeepAliveValues(m_Client, 10 * 1000, 1000);
-#endif
             }
             catch (PlatformNotSupportedException)
             {
-                // .net core on linux does not support modification of that settings at the moment. defaults applied.
+                // .NET on Linux does not support modification of that settings at the moment. Defaults applied.
             }
 
             m_Stream = m_Client.GetStream();
@@ -119,11 +77,7 @@ namespace InsightCore.Net
             if (m_UseSsl)
             {
                 m_SslStream = new SslStream(m_Stream);
-#if NETSTANDARD1_3
-                m_SslStream.AuthenticateAsClientAsync(ServerAddr).Wait();
-#else
                 m_SslStream.AuthenticateAsClient(ServerAddr);
-#endif
             }
         }
 
@@ -139,7 +93,7 @@ namespace InsightCore.Net
             {
                 try
                 {
-                    ((IDisposable)m_Client).Dispose();
+                    m_Client.Dispose();
                 }
                 catch
                 {
